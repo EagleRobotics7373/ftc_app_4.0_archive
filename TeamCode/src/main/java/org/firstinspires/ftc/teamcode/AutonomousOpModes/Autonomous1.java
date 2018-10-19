c68ed156f0e4551d0ee51f21b2af85f984295ab9
Luke S <lukespin21@gmail.com>
2018-10-19 16:31:16 -0400

    Tweaked Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpModes/Autonomous1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpModes/Autonomous1.java
@@ -50,34 +50,43 @@ public class Autonomous1 extends LinearOpMode {
 
         if (color_left.red > color_center.red && color_left.red > color_right.red) {
             //Hit left?
-            /*MotorPower(0, -1, 0, 1);
+            MotorPower(0, -1, 0, 1);
             sleep(1000);
-            encoderDrive(DRIVE_SPEED, -6, -6, -6, -6, 5);
-            */
+            encoderDrive(robot.DRIVE_SPEED, -6, -6, -6,
+                    -6, 0, 0, 5);
+
             telemetry.addLine("Hit left");
             telemetry.update();
             sleep(5000);
 
         } else if (color_center.red > color_left.red && color_center.red > color_right.red) {
             //Hit middle?
-            /*MotorPower(-1, -1, -1, -1);
+            MotorPower(-1, -1, -1, -1);
             sleep(100);
-            */
+
             telemetry.addLine("Hit center");
             telemetry.update();
             sleep(5000);
 
         } else if (color_right.red > color_center.red && color_right.red > color_left.red) {
             //Hit right?
-            /*MotorPower(0, 1, 0, -1);
+            MotorPower(0, 1, 0, -1);
             sleep(1000);
-            encoderDrive(DRIVE_SPEED, -6, -6,-6,-6, 5);
-            */
+            encoderDrive(robot.DRIVE_SPEED, -6, -6,-6,
+                    -6, 0, 0, 5);
+
             telemetry.addLine("Hit right");
             telemetry.update();
             sleep(5000);
         }
 
+    }
+
+    private void MotorPower(double frontright, double backright, double frontleft, double backleft) {
+        robot.frontright.setPower(frontright);
+        robot.backright.setPower(backright);
+        robot.frontleft.setPower(frontleft);
+        robot.backleft.setPower(backleft);
     }
 
     public void encoderDrive(double speed, double frontleftinches, double frontrightinches,
@@ -97,8 +106,8 @@ public class Autonomous1 extends LinearOpMode {
             backleftTarget = robot.backleft.getCurrentPosition() + (int) (backleftinches * robot.COUNTS_PER_INCH);
             frontrightTarget = robot.frontright.getCurrentPosition() + (int) (frontrightinches * robot.COUNTS_PER_INCH);
             backrightTarget = robot.backright.getCurrentPosition() + (int) (backrightinches * robot.COUNTS_PER_INCH);
-            rightliftTarget = robot.backright.getCurrentPosition() + (int) (rightliftinches * robot.COUNTS_PER_INCH);
-            leftliftTarget = robot.backright.getCurrentPosition() + (int) (leftliftinches * robot.COUNTS_PER_INCH);
+            rightliftTarget = robot.rightlift.getCurrentPosition() + (int) (rightliftinches * robot.COUNTS_PER_INCH);
+            leftliftTarget = robot.leftlift.getCurrentPosition() + (int) (leftliftinches * robot.COUNTS_PER_INCH);
 
             robot.frontleft.setTargetPosition(frontleftTarget);
             robot.backleft.setTargetPosition(backleftTarget);
@@ -133,7 +142,7 @@ public class Autonomous1 extends LinearOpMode {
             robot.frontright.setPower(0);
             robot.backright.setPower(0);
             robot.rightlift.setPower(0);
-            robot.rightlift.setPower(0);
+            robot.leftlift.setPower(0);
 
             robot.frontright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             robot.backright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
