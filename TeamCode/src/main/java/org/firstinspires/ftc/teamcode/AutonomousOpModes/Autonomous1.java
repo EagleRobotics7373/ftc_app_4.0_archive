3ba3aeefce1ca90c91dcc240535e9db1c87d7e97
Luke S <lukespin21@gmail.com>
2018-10-20 13:03:40 -0400

    Tweaked Autonomous and Tele-Op

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpModes/Autonomous1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpModes/Autonomous1.java
@@ -33,11 +33,13 @@ public class Autonomous1 extends LinearOpMode {
         robot.backright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
+        MotorPower(-1, -1, -1, -1);
+        sleep(15000);
         //encoderDrive(robot.DRIVE_SPEED, 10, 10, 10, 10, 5);
 
         //robot.leftlift.setPower(.85);
         //robot.rightlift.setPower(1);
-        sleep(13000);
+        /*sleep(13000);
 
         MotorPower(-1,-1,-1,-1);
         sleep(250);
@@ -50,21 +52,17 @@ public class Autonomous1 extends LinearOpMode {
         robot.backright.setPower(1);
         sleep(500);
 
-        MotorPower(0,0,0,0);
+        MotorPower(0,0,0,0);*/
 
         /*encoderDrive(robot.DRIVE_SPEED, 2, 2, 2, 2, 5);
-
         robot.frontleft.setPower(1);
         robot.backleft.setPower(1);
         robot.frontright.setPower(1);
         robot.backright.setPower(-1);
         sleep(1000);
-
         MotorPower(0,0,0,0);
-
         encoderDrive(robot.DRIVE_SPEED, 12, 12, 12,
                 12, 5);
-
         //robot.servoright.setPosition(.4);
         //robot.servoleft.setPosition(.4);
         sleep(1000);*/
@@ -73,95 +71,89 @@ public class Autonomous1 extends LinearOpMode {
         NormalizedRGBA color_center = robot.CScenter.getNormalizedColors();
         NormalizedRGBA color_right = robot.CSright.getNormalizedColors();
         sleep(1000);
-
         if (color_left.red > color_center.red && color_left.red > color_right.red) {
             //Hit left?
             MotorPower(0, -1, 0, 1);
             sleep(1000);
             encoderDrive(robot.DRIVE_SPEED, -6, -6, -6,
                     -6, 5);
-
             telemetry.addLine("Hit left");
             telemetry.update();
             sleep(5000);
             }
-
             else if (color_center.red > color_left.red && color_center.red > color_right.red) {
             //Hit middle?
             MotorPower(-1, -1, -1, -1);
             sleep(100);
-
             telemetry.addLine("Hit center");
             telemetry.update();
             sleep(5000);
             }
-
             else if (color_right.red > color_center.red && color_right.red > color_left.red) {
             //Hit right?
             MotorPower(0, 1, 0, -1);
             sleep(1000);
             encoderDrive(robot.DRIVE_SPEED, -6, -6, -6,
                     -6,  5);
-
             telemetry.addLine("Hit right");
             telemetry.update();
             sleep(5000);
             }*/
     }
-        private void MotorPower ( double frontright, double backright, double frontleft,
-        double backleft){
-            robot.frontright.setPower(frontright);
-            robot.backright.setPower(backright);
-            robot.frontleft.setPower(frontleft);
-            robot.backleft.setPower(backleft);
-        }
+    private void MotorPower ( double frontright, double backright, double frontleft,
+                              double backleft){
+        robot.frontright.setPower(frontright);
+        robot.backright.setPower(backright);
+        robot.frontleft.setPower(frontleft);
+        robot.backleft.setPower(backleft);
+    }
 
-        public void encoderDrive ( double speed, double frontleftinches, double frontrightinches,
-        double backleftinches, double backrightinches, double timeoutS){
+    public void encoderDrive ( double speed, double frontleftinches, double frontrightinches,
+                               double backleftinches, double backrightinches, double timeoutS){
 
-            int frontleftTarget;
-            int backleftTarget;
-            int frontrightTarget;
-            int backrightTarget;
+        int frontleftTarget;
+        int backleftTarget;
+        int frontrightTarget;
+        int backrightTarget;
 
-            if (opModeIsActive()) {
+        if (opModeIsActive()) {
 
-                frontleftTarget = robot.frontleft.getCurrentPosition() + (int) (frontleftinches * robot.COUNTS_PER_INCH);
-                backleftTarget = robot.backleft.getCurrentPosition() + (int) (backleftinches * robot.COUNTS_PER_INCH);
-                frontrightTarget = robot.frontright.getCurrentPosition() + (int) (frontrightinches * robot.COUNTS_PER_INCH);
-                backrightTarget = robot.backright.getCurrentPosition() + (int) (backrightinches * robot.COUNTS_PER_INCH);
+            frontleftTarget = robot.frontleft.getCurrentPosition() + (int) (frontleftinches * robot.COUNTS_PER_INCH);
+            backleftTarget = robot.backleft.getCurrentPosition() + (int) (backleftinches * robot.COUNTS_PER_INCH);
+            frontrightTarget = robot.frontright.getCurrentPosition() + (int) (frontrightinches * robot.COUNTS_PER_INCH);
+            backrightTarget = robot.backright.getCurrentPosition() + (int) (backrightinches * robot.COUNTS_PER_INCH);
 
-                robot.frontleft.setTargetPosition(frontleftTarget);
-                robot.backleft.setTargetPosition(backleftTarget);
-                robot.frontright.setTargetPosition(frontrightTarget);
-                robot.backright.setTargetPosition(backrightTarget);
+            robot.frontleft.setTargetPosition(frontleftTarget);
+            robot.backleft.setTargetPosition(backleftTarget);
+            robot.frontright.setTargetPosition(frontrightTarget);
+            robot.backright.setTargetPosition(backrightTarget);
 
-                robot.frontleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                robot.backleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                robot.frontright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                robot.backright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.frontleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.backleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.frontright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.backright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-                runtime.reset();
-                robot.frontleft.setPower(speed);
-                robot.backleft.setPower(speed);
-                robot.frontright.setPower(speed);
-                robot.backright.setPower(speed);
+            runtime.reset();
+            robot.frontleft.setPower(speed);
+            robot.backleft.setPower(speed);
+            robot.frontright.setPower(speed);
+            robot.backright.setPower(speed);
 
-                while (opModeIsActive() &&
-                        (runtime.seconds() < timeoutS) &&
-                        (robot.frontleft.isBusy() && robot.frontright.isBusy())) {
-                }
-
-                // Stop all motion
-                robot.frontleft.setPower(0);
-                robot.backleft.setPower(0);
-                robot.frontright.setPower(0);
-                robot.backright.setPower(0);
-
-                robot.frontright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                robot.backright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                robot.frontleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                robot.backleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (robot.frontleft.isBusy() && robot.frontright.isBusy())) {
             }
+
+            // Stop all motion
+            robot.frontleft.setPower(0);
+            robot.backleft.setPower(0);
+            robot.frontright.setPower(0);
+            robot.backright.setPower(0);
+
+            robot.frontright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.backright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.frontleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.backleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
     }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModes/Teleop1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModes/Teleop1.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.TeleOpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.HardwareRobot;
@@ -20,10 +19,10 @@ public class Teleop1 extends LinearOpMode {
 
         robot.init(hardwareMap);
 
-        robot.leftlift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        /*robot.leftlift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.rightlift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.leftlift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        robot.rightlift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightlift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);*/
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -36,21 +35,34 @@ public class Teleop1 extends LinearOpMode {
         while (opModeIsActive()) {
 
             robot.frontleft.setPower(gamepad1.left_stick_y);
-            robot.backleft.setPower(-gamepad1.left_stick_y);
+            robot.backleft.setPower(gamepad1.left_stick_y);
             robot.frontright.setPower(gamepad1.left_stick_y);
-            robot.backright.setPower(-gamepad1.left_stick_y);
+            robot.backright.setPower(gamepad1.left_stick_y);
 
-            robot.backleft.setPower(gamepad1.right_stick_x);
-            robot.frontleft.setPower(gamepad1.right_stick_x);
-            robot.backright.setPower(-gamepad1.right_stick_x);
-            robot.frontright.setPower(-gamepad1.right_stick_x);
+            robot.backleft.setPower(-gamepad1.right_stick_x);
+            robot.frontleft.setPower(-gamepad1.right_stick_x);
+            robot.backright.setPower(gamepad1.right_stick_x);
+            robot.frontright.setPower(gamepad1.right_stick_x);
 
-            robot.leftlift.setPower(-gamepad2.left_stick_y);
-            robot.rightlift.setPower(-gamepad2.right_stick_y);
+            //robot.grabber.setPower(gamepad2.right_stick_y * .2);
+            //robot.turn.setPower(gamepad2.left_stick_x * .2);
 
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.update();
+            /*if (gamepad2.x) {
+                robot.servoleft.setDirection(Servo.Direction.FORWARD);
+                robot.servoleft.setDirection(Servo.Direction.REVERSE);}
+            else if (gamepad2.b) {
+                robot.servoleft.setDirection(Servo.Direction.FORWARD);
+                robot.servoleft.setDirection(Servo.Direction.REVERSE);}*/
+
         }
+        //robot.leftlift.setPower(-gamepad2.left_stick_y);
+        //robot.rightlift.setPower(-gamepad2.right_stick_y);
+
+        //robot.servoright.setPosition(0);
+        //robot.servoleft.setPosition(.85);
+
+        // Show the elapsed game time and wheel power.
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.update();
     }
 }
